4ce2f1b74cc88d177aef6b251b4b52d4a977f5fa
jay-so <52352476+jay-so@users.noreply.github.com>
2022-09-26 22:04:55 +0900

    Add files via upload

---
+++ boj_10998.java
@@ -0,0 +1,22 @@
+package boj_step1;
+
+import java.util.Scanner;
+
+public class boj_10998 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if((a<=0)&&(a>=10)){
+            System.out.print("a에 1~9사이의 수를 넣어주세요!");
+        }
+
+        if((b<=0)&&(b>=10)){
+            System.out.print("b에 1~9사이의 수를 넣어주세요!");
+        }
+
+        System.out.print(a*b);
+
+    }
+}
