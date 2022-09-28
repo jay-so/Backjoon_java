ff1f719e816d5867ca71af076e068b29304fcb33
jay-so <52352476+jay-so@users.noreply.github.com>
2022-09-28 23:10:15 +0900

    Add files via upload

---
+++ boj_10869.java
@@ -0,0 +1,23 @@
+package boj_step1;
+import java.util.Scanner;
+
+public class boj_10869 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if((a<=0)&&(a>=10001)){
+            System.out.println("a에 1에서 10000 사이의 수를 입력해주세요");
+        }else if((b<=0)&&(b>=10001)){
+            System.out.println("b에 1에서 10000 사이의 수를 입력해주세요");
+        }else {
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(a%b);
+        }
+
+    }
+}
