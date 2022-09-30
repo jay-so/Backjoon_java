4d4bf8d07835edf6b781057d89447567ebea9074
jay-so <52352476+jay-so@users.noreply.github.com>
2022-09-30 21:33:41 +0900

    Add files via upload

---
+++ boj_10171.java
@@ -0,0 +1,10 @@
+package boj_step1;
+
+public class boj_10171 {
+    public static void main(String[]args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ boj_10172.java
@@ -0,0 +1,11 @@
+package boj_step1;
+
+public class boj_10172 {
+    public static void main(String[]args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
