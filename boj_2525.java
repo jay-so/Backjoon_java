07c1fabc86685d054a1e08c0f6aba8f9a41089c5
jay-so <52352476+jay-so@users.noreply.github.com>
2022-10-02 16:48:51 +0900

    Add files via upload

---
+++ boj_2525.java
@@ -0,0 +1,24 @@
+package boj_step2;
+
+import java.util.Scanner;
+
+public class boj_2525 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int value = sc.nextInt();
+
+        m += value;
+        if (m >= 60) {
+            while (m >= 60) {
+                m -= 60;
+                h++;
+                if (h >= 24) {
+                    h = 0;
+                }
+            }
+        }
+        System.out.println(h + " " + m);
+    }
+}
