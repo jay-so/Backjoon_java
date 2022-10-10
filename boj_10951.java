239cc9c5ef0602dd33619f1ce5ba5de2caa7da0c
jay-so <52352476+jay-so@users.noreply.github.com>
2022-10-10 18:18:01 +0900

    Add files via upload

---
+++ boj_10951.java
@@ -0,0 +1,17 @@
+package boj_step3;
+
+import java.util.Scanner;
+
+public class boj_10951 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextInt()){
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a+b);
+        }
+        scanner.close();
+    }
+}
