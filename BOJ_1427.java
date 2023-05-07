3491aa995b5b0245c80b9a07963d616a63836bdc
jay-so <52352476+jay-so@users.noreply.github.com>
2023-05-07 21:49:45 +0900

    Add files via upload

---
+++ BOJ_1427.java
@@ -0,0 +1,23 @@
+package boj_sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BOJ_1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int n = s.length();
+        Integer[] arr = new Integer[n];  //내림차순으로 정렬할 배열. int형이 아닌 Integer형으로 선언
+        for (int i = 0; i < n; i++) {
+            arr[i] = Character.getNumericValue(s.charAt(i));  //배열에 수 저장
+        }
+        Arrays.sort(arr, Collections.reverseOrder());  //내림차순으로 정렬
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
