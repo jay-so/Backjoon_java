f60e552bfef0d075921114d1386b003df492f2a7
jay-so <52352476+jay-so@users.noreply.github.com>
2023-07-01 21:55:49 +0900

    백준 1541번 풀이

---
+++ BOJ_1541.java
@@ -0,0 +1,28 @@
+package boj_sliver;
+
+import java.util.Scanner;
+
+public class BOJ_1541 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int sum = Integer.MAX_VALUE;
+        String[] subtraction = in.nextLine().split("-");
+
+
+        for(int i = 0; i < subtraction.length; i++) {
+            int temp = 0;
+            String[] addition = subtraction[i].split("\\+");
+            for(int j = 0; j < addition.length; j++) {
+                temp += Integer.parseInt(addition[j]);
+            }
+            if (sum == Integer.MAX_VALUE) {
+                sum = temp;
+            } else {
+                sum -= temp;
+            }
+        }
+        System.out.println(sum);
+    }
+}
